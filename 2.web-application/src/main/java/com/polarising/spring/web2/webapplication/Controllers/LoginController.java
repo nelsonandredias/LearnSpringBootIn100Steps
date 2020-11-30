85552776b6572aa0229fcb1f56653a633ebfd4fc
nelson <srandrediaz@gmail.com>
2020-11-30 16:36:53 +0000

    added example with jsp views and string response bodys

---
+++ 2.web-application/src/main/java/com/polarising/spring/web2/webapplication/Controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.polarising.spring.web2.webapplication.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/login")
+	@ResponseBody
+	public String loginMessage() {
+		return "Hello World Modified";
+	}
+	
+	@RequestMapping("/greetings")
+	public String greetingsMessage() {
+		return "greetings";
+	}
+}
