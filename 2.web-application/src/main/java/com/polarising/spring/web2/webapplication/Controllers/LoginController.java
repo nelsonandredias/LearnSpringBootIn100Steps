2c086e10f931220d8ffa9d9b26f2ce238f83b2b8
nelson <srandrediaz@gmail.com>
2020-11-30 23:34:45 +0000

    added different HTTP methods. created a new jsp page and passing values from one jsp page to another jsp page

--- 2.web-application/src/main/java/com/polarising/spring/web2/webapplication/Controllers/LoginController.java
+++ 2.web-application/src/main/java/com/polarising/spring/web2/webapplication/Controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.polarising.spring.web2.webapplication.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,16 +11,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
 	
-	
-	
-	@RequestMapping("/login")
+	@RequestMapping(value="/test", method = RequestMethod.GET)
 	@ResponseBody
-	public String loginMessage() {
-		return "Hello World Modified";
+	public String testMessage() {
+		return "this is a test message";
 	}
 	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage() {
+		return "login";
+	}
 	
-	@RequestMapping("/greetings")
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String showWelcomePage(@RequestParam String fName, ModelMap model) {
+		
+		System.out.println("fName--------------> " + fName);
+		// Model is used to pass data from Controller to View (JSP)
+		model.put("mName", fName);
+		return "welcome";
+	}
+	
+	@RequestMapping(value="/greetings", method = RequestMethod.GET)
 	public String greetingsMessage(@RequestParam String name, ModelMap model) {
 		
 		// Model is used to pass data from Controller to View (JSP)
---
+++ 2.web-application/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>First Web Application</title>
+</head>
+<body>
+<form method="POST">
+	Name: <input type="text" name="fName"/>
+	Password: <input type="password" name="password"/>
+	<input type="submit"/>
+</form>
+</body>
+</html>
